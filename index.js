8ab65a68a4e7ecee8b37475794a5e0302ac5fe52
snowdreams1006 <snowdreams1006@163.com>
2019-10-03 22:36:53 +0800

    fix bug

--- index.js
+++ index.js
@@ -20,16 +20,8 @@ module.exports = {
             this.log.debug.ln('page:before', page.content);
 
             if (copyrightConfig) {
-                var copyright = ` 
-                    ```
-作者: ${copyrightConfig.author}
-链接: ${copyrightConfig.site}
-来源: ${copyrightConfig.website}
-本文原创发布于${copyrightConfig.website},©著作权归作者所有,转载请注明出处,谢谢合作!
-```
-
-![${copyrightConfig.image}](${copyrightConfig.image})
-                `;
+                var copyright = '\n```html\n作者: '+copyrightConfig.author+'\n链接:'+copyrightConfig.site+'\n来源: '+copyrightConfig.website+'\n本文原创发布于'+copyrightConfig.website+',©著作权归作者所有,转载请注明出处,谢谢合作!```\n';
+                copyright += `\n![${copyrightConfig.image}](${copyrightConfig.image})`;
 
                 page.content = page.content + '\n' + copyright;
             }
