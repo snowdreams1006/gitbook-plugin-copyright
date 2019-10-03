c7e0d74aafdbf4c21f00c0b02bcfb4e5f725b778
snowdreams1006 <snowdreams1006@163.com>
2019-10-03 22:18:31 +0800

    remoeve `

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ module.exports = {
 
             if (copyrightConfig) {
                 var copyright = ` 
-                \`\`\`html
+                    ```
 作者: ${copyrightConfig.author}
 链接: ${copyrightConfig.site}
 来源: ${copyrightConfig.website}
 本文原创发布于${copyrightConfig.website},©著作权归作者所有,转载请注明出处,谢谢合作!
-\`\`\`
+```
 
 ![${copyrightConfig.image}](${copyrightConfig.image})
-`;
+                `;
 
                 page.content = page.content + '\n' + copyright;
             }
