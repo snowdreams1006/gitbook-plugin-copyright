942a456d435d2732d196e63de4395413862a4a25
snowdreams1006 <snowdreams1006@163.com>
2019-10-03 20:09:12 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var copyrightConfig = {};
+
+module.exports = {
+    book: {
+        assets: "./assets",
+        css: [
+            "copyright.css"
+        ],
+        js: [
+            "copyright.js"
+        ]
+    },
+    hooks: {
+        "init": function() {
+            this.log.debug.ln('init', this.options.pluginsConfig.copyright);
+
+            copyrightConfig = this.options.pluginsConfig.copyright;
+        },
+        "page:before": function(page) {
+            this.log.debug.ln('page:before', page.content);
+
+            if (copyrightConfig) {
+                var str = '<div id="copyright-container" class="gitbook-plugin-copyright"></div>';
+
+                page.content = page.content + '\n' + str;
+            }
+
+            return page;
+        }
+    }
+};
