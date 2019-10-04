b67b0fabe51cfa1c026b3481451abe9e670d1435
snowdreams1006 <snowdreams1006@163.com>
2019-10-04 23:18:48 +0800

    this.options.langure

--- index.js
+++ index.js
@@ -14,25 +14,25 @@ module.exports = {
             copyrightConfig = this.options.pluginsConfig.copyright;
         },
         "page:before": function(page) {
-            this.log.debug.ln('page:before', JSON.stringify(page));
+            this.log.debug.ln('page:before', JSON.stringify(page.content));
 
             if (copyrightConfig) {
                 var site = copyrightConfig.site;
                 if (site.slice(-1) != "/") {
                     site += '/';
                 }
-
-// var lang = gitbook.state.innerLanguage;
-                console.log(`isLanguageBook=${this.isLanguageBook()},language=${this.book.language}`);
-
+                var lang = this.options.language;
+                if (lang) {
+                    lang += '/';
+                }
                 var url = page.path;
                 var readmeReg = /\/?\bREADME\.md$/;
                 if (readmeReg.test(url)) {
-                    url = site + ( url === 'README.md' ? '' : url.replace(readmeReg, '/'));
+                    url = site + lang + (url === 'README.md' ? '' : url.replace(readmeReg, '/'));
                 } else {
-                    url = site + url.replace(/.md$/, '.html');
+                    url = site + lang + url.replace(/.md$/, '.html');
                 }
-                var copyright = '\n\n```html\n作者: ' + copyrightConfig.author + '\n链接: ' + url + '\n来源: ' + copyrightConfig.website + '\n本文原创发布于「' + copyrightConfig.website + '」,转载请注明出处,谢谢合作!```\n';
+                var copyright = '\n\n```html\n作者: ' + copyrightConfig.author + '\n链接: ' + url + '\n来源: ' + copyrightConfig.website + '\n本文原创发布于「' + copyrightConfig.website + '」,转载请注明出处,谢谢合作!\n```\n';
                 if (copyrightConfig.image) {
                     copyright += `\n![${copyrightConfig.image}](${copyrightConfig.image})`;
                 }
